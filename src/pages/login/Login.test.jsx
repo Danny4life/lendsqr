e6991396cebf8f24c00414b45c0f3cb3ee93f755
Segun Osiki <osiki.segun@gmail.com>
2022-12-25 20:30:20 +0100

    wrote test for password input

--- src/pages/login/Login.test.jsx
+++ src/pages/login/Login.test.jsx
@@ -5,4 +5,10 @@ test("Email input should be rendered", () => {
     render(<Login />);
     const userEmailEl = screen.getByPlaceholderText(/Email/i);
     expect(userEmailEl).toBeInTheDocument();
+});
+
+test("Password input should be rendered", () => {
+    render(<Login />);
+    const userPasswordEl = screen.getByPlaceholderText(/Password/i);
+    expect(userPasswordEl).toBeInTheDocument();
 })
