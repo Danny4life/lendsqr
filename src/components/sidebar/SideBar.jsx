156775b70c993e1fd046bdf5618e9476f91b7aed
Segun Osiki <osiki.segun@gmail.com>
2022-12-26 19:44:09 +0100

    added sidebar component

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,11 @@
+import "./sidebar.scss"
+
+const SideBar = () => {
+    return ( 
+        <div className="sidebar">
+            hello sidebar
+        </div>
+     );
+}
+ 
+export default SideBar;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,14 @@
 import NavBar from "../../components/navbar/NavBar";
+import SideBar from "../../components/sidebar/SideBar";
 import "./dashboard.scss"
 
 const Dashboard = () => {
     return ( 
         <div className="dashboard">
             <NavBar />
+            <div className="dashboard-container">
+                <SideBar />
+            </div>
 
         </div>
      );
