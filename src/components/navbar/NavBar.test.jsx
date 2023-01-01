646fd1b29f66306b25b4d7d9061b7b20120f2546
Segun Osiki <osiki.segun@gmail.com>
2023-01-01 04:38:07 +0100

    wrote test for Navbar logo text

--- src/components/navbar/NavBar.test.jsx
+++ src/components/navbar/NavBar.test.jsx
@@ -2,8 +2,15 @@ import { render, screen } from "@testing-library/react";
 import NavBar from "./NavBar";
 
 
-test("search bar input should rendered", () => {
+test("should rendered Navbar search input field", () => {
     render(<NavBar />);
     const userSearchBar = screen.getByPlaceholderText(/Search for anything/i);
     expect(userSearchBar).toBeInTheDocument();
+})
+
+test("should rendered Navbar logo text", () => {
+    render(<NavBar />);
+    const navbarLogoText = screen.getByText(/lendsqr/i);
+    expect(navbarLogoText).toBeInTheDocument();
+   
 })
