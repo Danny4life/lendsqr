c48a42b945e78f54ae91ccc66d998770f840aba9
Segun Osiki <osiki.segun@gmail.com>
2023-01-14 11:16:27 +0100

    Routed user page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Login from "./pages/login/Login";
+import UserPage from "./pages/userpage/UserPage";
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/lendsqr" element={ <Login />} />
           <Route path="/dashboard" element={ <Dashboard />} />
+          <Route path="/userPage" element={ <UserPage />} />
         </Routes>
       </BrowserRouter>
     
