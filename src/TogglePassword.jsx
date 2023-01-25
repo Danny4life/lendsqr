8d9f6272a867d19e7927cb4468cf47e8c39037fa
Segun Osiki <osiki.segun@gmail.com>
2023-01-25 08:57:29 +0100

    add media quaries for mobile sm

--- src/TogglePassword.jsx
+++
@@ -1,9 +0,0 @@
-// import { useState } from "react";
-
-// const TogglePassword = () => {
-
-//     const[visible, setVisible] = useState(false);
-//     return []
-// }
- 
-// export default TogglePassword;
