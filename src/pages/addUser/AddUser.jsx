5722d6c8c711ef78ae12b9b45b47fee33fa967c1
Segun Osiki <osiki.segun@gmail.com>
2023-01-26 22:49:46 +0100

    add navbar and sidebar to add user comoponent

--- src/pages/addUser/AddUser.jsx
+++ src/pages/addUser/AddUser.jsx
@@ -1,7 +1,27 @@
+import NavBar from "../../components/navbar/NavBar";
+import SideBar from "../../components/sidebar/SideBar";
+import "./adduser.scss";
+
 const AddUser = () => {
     return ( 
-        <div>
-            hello user
+        <div className="addUser">
+            <nav>
+                <NavBar />
+            </nav>
+
+            <section className="addUser-container">
+                <div className="left">
+                    <aside>
+                        <SideBar />
+                    </aside>
+                </div>
+
+                <div className="right">
+                    
+                </div>
+
+            </section>
+           
         </div>
      );
 }
