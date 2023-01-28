b834ffe2fafcd1c70faa5307dc280c9226f188f3
Segun Osiki <osiki.segun@gmail.com>
2023-01-28 11:10:27 +0100

    add user detials api

--- src/pages/addUser/AddUser.jsx
+++ src/pages/addUser/AddUser.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
 import NavBar from "../../components/navbar/NavBar";
 import SideBar from "../../components/sidebar/SideBar";
+import UserService from "../../services/UserService";
 import "./adduser.scss";
 
 const AddUser = () => {
+
+    const [userDetails, setUserDetails] = useState({
+        id : "",
+        fullName : "",
+        phoneNumber : "",
+        email : "",
+        bvn : "",
+        gender : "",
+        status : "",
+        child : "",
+        residence : "",
+    });
+
+    const handleChange = (e) => {
+
+        const value = e.target.value;
+        setUserDetails({ ...userDetails, [e.target.name] : value });
+
+    }
+
+    const saveUserDetails = (e) => {
+        e.preventDefault();
+        UserService.saveUserDetails(userDetails).then((response) => {
+            setUserDetails(response);
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+    }
     return ( 
         <div className="addUser">
             <nav>
@@ -27,36 +60,90 @@ const AddUser = () => {
                                     <legend>Personal Information</legend>
                                     <div className="form-control-1">
                                         <label htmlFor="fullName">Full Name</label>
-                                        <input type="text" id="fullName" name="fullName" required />
+                                        <input 
+                                        type="text" 
+                                        id="fullName" 
+                                        name="fullName" 
+                                        value={userDetails.fullName}
+                                        onChange={(e) => handleChange(e)}
+                                        required />
 
                                         <label htmlFor="phoneNumber">phone Number</label>
-                                        <input type="number" id="phoneNumber" name="phoneNumber" required />
+                                        <input 
+                                        type="number" 
+                                        id="phoneNumber" 
+                                        name="phoneNumber" 
+                                        value={userDetails.phoneNumber}
+                                        onChange={(e) => handleChange(e)}
+                                        required />
 
                                         <label htmlFor="email">Email</label>
-                                        <input type="email" id="email" name="email" required />
+                                        <input 
+                                        type="email" 
+                                        id="email" 
+                                        name="email"
+                                        value={userDetails.email}
+                                        onChange={(e) => handleChange(e)} 
+                                        required />
                                     </div>
 
                                     <div className="form-control-2">
                                         <label htmlFor="bvn">BVN</label>
-                                        <input className="bvn-input" type="number" id="bvn" name="bvn" required />
+                                        <input 
+                                        className="bvn-input" 
+                                        type="number" 
+                                        id="bvn" 
+                                        name="bvn"
+                                        value={userDetails.bvn}
+                                        onChange={(e) => handleChange(e)}
+                                         required />
 
                                         <label htmlFor="gender">Gender</label>
-                                        <input className="gender" type="text" id="gender" name="gender" />
+                                        <input 
+                                        className="gender" 
+                                        type="text" 
+                                        id="gender" 
+                                        name="gender"
+                                        value={userDetails.gender}
+                                        onChange={(e) => handleChange(e)} />
 
                                         <label className="label-status" htmlFor="status">Marital Status</label>
-                                        <input className="status" type="text" id="status" name="status" />
+                                        <input 
+                                        className="status" 
+                                        type="text" 
+                                        id="status" 
+                                        name="status" 
+                                        value={userDetails.status}
+                                        onChange={(e) => handleChange(e)} />
                                     </div>
 
                                     <div className="form-control-3">
                                         <label htmlFor="child">Children</label>
-                                        <input className="child-input" type="text" id="child" name="child" />
+                                        <input 
+                                        className="child-input" 
+                                        type="text" 
+                                        id="child" 
+                                        name="child"
+                                        value={userDetails.child}
+                                        onChange={(e) => handleChange(e)} />
 
                                         <label htmlFor="residece">Type of Residence</label>
-                                        <input type="text" id="residence" name="residence" required />
+                                        <input 
+                                        type="text" 
+                                        id="residence" 
+                                        name="residence" 
+                                        value={userDetails.residence}
+                                        onChange={(e) => handleChange(e)}
+                                        required />
                                     </div>
 
                                     <div className="submit">
-                                         <button className="save-btn">Save</button>
+                                        <button 
+                                         className="save-btn"
+                                         onClick={(e) => saveUserDetails(e)}
+                                         >
+                                            Save
+                                        </button>
                                          <button className="clear-btn">Clear</button>
                                     </div>
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const USER_API_BASE_URL = "http://localhost:8080/api/v1/users";
+const USERDETAILS_API_BASE_URL = "http://localhost:8080/api/v1/userDetails";
 
 
 
@@ -8,6 +9,10 @@ class UserService {
 
     saveUser(user){
         return axios.post(USER_API_BASE_URL, user);
+    }
+
+    saveUserDetails(userDetails){
+        return axios.post(USERDETAILS_API_BASE_URL, userDetails);
     }
 
 }
